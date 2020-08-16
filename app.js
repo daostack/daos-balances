5c06ab69f765b0ad567235a24521f7326f5082db
Roie Natan <roie.natan@gmail.com>
2020-08-16 22:11:12 +0300

    allowCrossDomain

--- app.js
+++ app.js
@@ -13,8 +13,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-requested-with, authorization');
+  next();
+}
+
 app.use(logger('dev'));
 app.use(express.json());
+app.use(allowCrossDomain);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
